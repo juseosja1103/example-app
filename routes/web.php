2d8e6916c54a17a957fb32d61c700a079568218e
juseosja1103 <sebasosoriojaramillo@gmail.com>
2024-06-06 20:44:56 -0500

    repo

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts',function(){
+    return "Aqui  se mostrarán todos los post";
+});
+Route::get('/posts',function(){ 
+    return "aquí va mi script";
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
